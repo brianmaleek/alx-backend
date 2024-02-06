3eff2bac680275632e009b3e9afda55023ae2c02
maleekmalik@gmail.com <maleekmalik@gmail.com>
2024-02-06 23:47:26 +0300

    4-app.py -- corrected render template html file

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -41,7 +41,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index():
     """Returns a string"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
